819c2022a1eee82a213959a3597ac10b1d5f32f4
trannhattan25 <tanwaifu125@gmail.com>
2025-08-05 22:23:44 +0700

    #35. Design NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,20 +4,8 @@ const connection = require('../config/database');
 let users = [];
 
 const getHomePage = (req, res) => {
+    return res.render('home.ejs');
 
-    // A simple SELECT query
-    connection.query(
-        'select * from Users u',
-        function (err, results, fields) {
-            users = results;
-
-            console.log(">>> result: ", results); // results contains rows returned by server
-            console.log(">>> check users", users);
-
-            res.send(JSON.stringify(users))
-
-        }
-    );
 }
 const getNhattan = (req, res) => {
     res.render('sample.ejs')
