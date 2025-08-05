e10c3b0bcd5c7cd6cdd9c61d0438fd58ceb59968
trannhattan25 <tanwaifu125@gmail.com>
2025-08-05 17:51:44 +0700

    #31. Tái Sử Dụng Connection

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ require('dotenv').config()
 const express = require('express');// common js
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./routers/web');
-
-//GET THE CLIENT
-const mysql = require('mysql2')
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -18,20 +16,12 @@ configViewEngine(app);
 //khai bao route
 app.use('/', webRoute)
 
-// Create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, // default: 3306
-    user: 'root',
-    password: '123456',// default : empty
-    database: 'hoidanit',
-});
+
 // A simple SELECT query
 connection.query(
     'select * from Users u',
     function (err, results, fields) {
         console.log(">>> result: ", results); // results contains rows returned by server
-        console.log(">>> fields: ", fields); // fields contains extra meta data about results, if available
     }
 );
 
