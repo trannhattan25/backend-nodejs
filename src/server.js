1adb6d6e87899a46fff0d671b8ec505a62a1d871
trannhattan25 <tanwaifu125@gmail.com>
2025-08-05 17:36:49 +0700

    #30. Sử Dụng MySQL với Node.JS

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const express = require('express');// common js
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./routers/web');
 
+//GET THE CLIENT
+const mysql = require('mysql2')
+
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -14,9 +17,23 @@ configViewEngine(app);
 
 //khai bao route
 app.use('/', webRoute)
-// app.use('/v1', webRoute)
-// app.use('/v2', webRoute)
 
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, // default: 3306
+    user: 'root',
+    password: '123456',// default : empty
+    database: 'hoidanit',
+});
+// A simple SELECT query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(">>> result: ", results); // results contains rows returned by server
+        console.log(">>> fields: ", fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 app.listen(port, () => {
